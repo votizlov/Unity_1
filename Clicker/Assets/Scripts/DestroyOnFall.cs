ae6a87b0f9225e3efab7fb7fb6e7637f1311e600
Votizlov <SeriousFox@yandex.tu>
2020-03-06 08:50:27 +0300

    Added destruction on fall

---
+++ Clicker/Assets/Scripts/DestroyOnFall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnFall : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.y < -4)
+            Destroy(gameObject);
+    }
+}
