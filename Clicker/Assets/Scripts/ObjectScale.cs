ae4ac6ff71d3aee1b08db37ec4286168507eed69
Votizlov <SeriousFox@yandex.tu>
2020-03-09 20:00:28 +0300

    Added pop if scaled too much
    Almost forgot...

--- Clicker/Assets/Scripts/ObjectScale.cs
+++ Clicker/Assets/Scripts/ObjectScale.cs
@@ -2,10 +2,13 @@
 
 public class ObjectScale : MonoBehaviour
 {
-    public float k = 1.1f;
+    [Header("Scale per frame")] public float k = 1.1f;
+    [Header("Scale k when destroyed")] public float kDestroy = 2;
 
     private void Update()
     {
         gameObject.transform.localScale *= k;
+        if (gameObject.transform.localScale.x > kDestroy)
+            Destroy(gameObject);
     }
 }
